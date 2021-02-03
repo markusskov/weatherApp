572441f6018d0f1a5035f09d5c5a3eeaa6f6a015
markusskov <69841783+markusskov@users.noreply.github.com>
2021-02-03 01:55:35 +0100

    It's a good idea to close tags.

--- js/script.js
+++ js/script.js
@@ -8,9 +8,9 @@ function getWeather() {
       document.querySelector('.oversikt').innerHTML = `
       <h1>Været i dag</h1>
       <h2>${data.name}:</h2> 
-      <p>Det er: ${data.main.temp} grader nå og ${data.weather[0].description}, men det føles som ${data.main.feels_like} grader.
-      <p>Makstemperaturen var ${data.main.temp_max} og det laveste var ${data.main.temp_min}.
-      <p>Vindstyrken er på ${data.wind.speed} km/t. 
+      <p>Det er: ${data.main.temp} grader nå og ${data.weather[0].description}, men det føles som ${data.main.feels_like} grader.</p>
+      <p>Makstemperaturen var ${data.main.temp_max} og det laveste var ${data.main.temp_min}.</p>
+      <p>Vindstyrken er på ${data.wind.speed} km/t.</p>
 
       `;
     });
