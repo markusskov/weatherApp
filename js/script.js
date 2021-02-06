28f4b8314895c53d707aca3be51c04b5b4ade914
markusskov <69841783+markusskov@users.noreply.github.com>
2021-02-06 17:54:38 +0100

    weatherApp 0.3
    - Added glass layout.
    - Added API for Molde and Trondheim.

--- js/script.js
+++ js/script.js
@@ -10,19 +10,68 @@ function getWeather() {
       locationIcon.innerHTML = `<img src="icons/${icon}.png"></img>`;
 
       document.querySelector('.grader').innerHTML = `
-      <h3>Oslo, Norge</h3> 
       <div class="grader_font">${Math.round(data.current.temp)}°</div>
       `;
 
-      document.querySelector('.vind').innerHTML = `
-      <p>Vindstyrke:</p><p class="bold">${data.current.wind_speed} m/s</p>
+      document.querySelector('.vind').innerHTML += ` 
+      <p class="bold">${data.current.wind_speed} m/s</p>
       `;
 
-      document.querySelector('.fuktighet').innerHTML = `
-      <p>Fuktighet:</p><p class="bold">${data.current.humidity} %</p>
+      document.querySelector('.fuktighet').innerHTML += `
+      <p class="bold">${data.current.humidity} %</p>
       `;
     });
 }
 getWeather();
 
-// .then((data) => console.log(Math.round(data.current.temp)))
+function getWeatherMolde() {
+  fetch(
+    'https://api.openweathermap.org/data/2.5/onecall?lat=62.7372&lon=7.1607&appid=c00a488091e665d81db55d4e2948cf0d&units=metric'
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      let locationIcon = document.querySelector('.weather-icon-molde');
+      const icon = data.current.weather[0].icon;
+      locationIcon.innerHTML = `<img src="icons/${icon}.png"></img>`;
+
+      document.querySelector('.grader-molde').innerHTML = `
+      <div class="grader_font">${Math.round(data.current.temp)}°</div>
+      `;
+
+      document.querySelector('.vind-molde').innerHTML += ` 
+      <p class="bold">${data.current.wind_speed} m/s</p>
+      `;
+
+      document.querySelector('.fuktighet-molde').innerHTML += `
+      <p class="bold">${data.current.humidity} %</p>
+      `;
+    });
+}
+getWeatherMolde();
+
+function getWeatherTrondheim() {
+  fetch(
+    'https://api.openweathermap.org/data/2.5/onecall?lat=63.4305&lon=10.3951&appid=c00a488091e665d81db55d4e2948cf0d&units=metric'
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      let locationIcon = document.querySelector('.weather-icon-trondheim');
+      const icon = data.current.weather[0].icon;
+      locationIcon.innerHTML = `<img src="icons/${icon}.png"></img>`;
+
+      document.querySelector('.grader-trondheim').innerHTML = `
+      <div class="grader_font">${Math.round(data.current.temp)}°</div>
+      `;
+
+      document.querySelector('.vind-trondheim').innerHTML += ` 
+      <p class="bold">${data.current.wind_speed} m/s</p>
+      `;
+
+      document.querySelector('.fuktighet-trondheim').innerHTML += `
+      <p class="bold">${data.current.humidity} %</p>
+      `;
+    });
+}
+getWeatherTrondheim();
